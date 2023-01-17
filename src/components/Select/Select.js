4b1a31bde634352e703a5b0b21f2660ee3ab9849
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2023-01-16 21:16:56 -0800

    head selector added

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './Select.css';
 
-export default function Select() {
+export default function Select({ head, setHead }) {
   return (
     <div className="select">
       <h1>Pick a head</h1>
-      <option value="dog">Dog</option>
-      <option value="duck">Duck</option>
-      <option value="horse">Horse</option>
-      <option value="bird">Bird</option>
+      <select
+        value={head}
+        onChange={(e) => {
+          setHead(e.target.value);
+        }}
+      >
+        <option value="dog">Dog</option>
+        <option value="duck">Duck</option>
+        <option value="horse">Horse</option>
+        <option value="bird">Bird</option>
+      </select>
     </div>
   );
 }
